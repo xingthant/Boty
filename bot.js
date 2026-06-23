9d1ac909b59ecea45a12d918167195035fc2d4c5
Xerry May <xerlokxerlok322@gmail.com>
2026-06-23 11:10:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const fs = require("fs");
 const express = require("express");
 
-const TOKEN = "8881474832:AAHEGCGjckQsXI1HZ3MDZzeC372AkG9MKBg";
+const TOKEN = "8881474832:AAHEGCGjckQsXI1HZ3MDZzKBg";
 const OWNER_ID = 7756391343;
 
 const bot = new TelegramBot(TOKEN, {
